fc76226c93dc29cb3731721279cba17e724f30c1
rachitk29 <rachitkumar2953@gmail.com>
2025-07-26 16:42:23 +0530

    add new files

--- 1300/3.queueAtTheSchool.cpp
+++ 1300/3.queueAtTheSchool.cpp
@@ -12,7 +12,7 @@ int main() {
         for (int i = 0; i < n - 1; ++i) {
             if (s[i] == 'B' && s[i + 1] == 'G') {
                 swap(s[i], s[i + 1]);
-                i++; // Skip next index to prevent re-swapping
+                i++; 
             }
         }
     }
